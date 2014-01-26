03e955f1329cead8690ea1c46f2f61a3f1759ce9
Nakul Javali <javali@usc.edu>
2014-01-26 00:33:48 -0800

    updated

--- r1.c
+++ r1.c
@@ -2,52 +2,57 @@
 #include <pcap.h>
 #include <pthread.h>
 
-void processor(u_char *arg,const struct pcap_pkthdr* pkthdr,const u_char* packet)
-{
-        int i=0, *counter = (int *)arg;
+#define SNAP_LEN 1518
+#define SIZE_ETHERNET 14
+#define ETHER_ADDR_LEN	6
 
-        printf("Packet Count: %d\n", ++(*counter));
-        printf("Received Packet Size: %d\n",pkthdr->len);
-        printf("Payload:\n");
+struct sniff_ethernet {
+        u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
+        u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
+        u_short ether_type;                     /* IP? ARP? RARP? etc */
+};
 
-        for(i=0; i <pkthdr->len;i++){
+struct sniff_ip {
+        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+        u_char  ip_tos;                 /* type of service */
+        u_short ip_len;                 /* total length */
+        u_short ip_id;                  /* identification */
+        u_short ip_off;                 /* fragment offset field */
+        #define IP_RF 0x8000            /* reserved fragment flag */
+        #define IP_DF 0x4000            /* dont fragment flag */
+        #define IP_MF 0x2000            /* more fragments flag */
+        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+        u_char  ip_ttl;                 /* time to live */
+        u_char  ip_p;                   /* protocol */
+        u_short ip_sum;                 /* checksum */
+        struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
 
-        if(isprint(packet[i]))
-        printf("%c", packet[i]);
-        else
-        printf(".");
+typedef u_int tcp_seq;
 
-        if((i%16==0 && i!=0)||i==pkthdr->len-1)
-        printf("\n");
-        
-	}
-
-}
-
-void *capture()
-{
-        int count=0;
-        char *dev, errbuf[PCAP_ERRBUF_SIZE];
-        pcap_t *handle;
-
-        dev = pcap_lookupdev(errbuf);
-
-        if(dev == NULL){
-        printf("No device found \n");
-        printf("Error: %s\n",errbuf);
-        return 0;
-        }
-
-        printf("Device found at: %s \n",dev);
-
-
-        handle = pcap_open_live(dev,2048,1,512,errbuf);
-
-        printf("here");
-        pcap_loop(handle,-1,processor,(u_char*) &count);
-
-        return 0;
-}
+struct sniff_tcp {
+        u_short th_sport;               /* source port */
+        u_short th_dport;               /* destination port */
+        tcp_seq th_seq;                 /* sequence number */
+        tcp_seq th_ack;                 /* acknowledgement number */
+        u_char  th_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+        u_char  th_flags;
+        #define TH_FIN  0x01
+        #define TH_SYN  0x02
+        #define TH_RST  0x04
+        #define TH_PUSH 0x08
+        #define TH_ACK  0x10
+        #define TH_URG  0x20
+        #define TH_ECE  0x40
+        #define TH_CWR  0x80
+        #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+        u_short th_win;                 /* window */
+        u_short th_sum;                 /* checksum */
+        u_short th_urp;                 /* urgent pointer */
+};
 
 
 void *printer()
@@ -81,24 +86,38 @@ void processor(u_char *arg,const struct pcap_pkthdr* pkthdr,const u_char* packet
 {
         int i=0, *counter = (int *)arg;
 
-        printf("Packet Count: %d\n", ++(*counter));
-        printf("Received Packet Size: %d\n",pkthdr->len);
-        printf("Payload:\n");
+	const struct sniff_ethernet *ethernet; 
+	const struct sniff_ip *ip;             
+	const struct sniff_tcp *tcp;           
+	const char *payload;                   
 
-        for(i=0; i <pkthdr->len;i++){
-
-        if(isprint(packet[i]))
-        printf("%c", packet[i]);
-        else
-        printf(".");
-   		
-		}
-
-
-
-
+	int size_ip;
+	int size_tcp;
+	int size_payload;
+	
+	ethernet = (struct sniff_ethernet*)(packet);
+	
+	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+	size_ip = IP_HL(ip)*4;
+	if (size_ip < 20) {
+		printf("   * Invalid IP header length: %u bytes\n", size_ip);
+		return;
 	}
 
+	switch(ip->ip_p) {
+		case IPPROTO_TCP:
+			count_tcp++;
+			break;
+		case IPPROTO_UDP:
+			count_ucp++;
+			return;
+		case IPPROTO_ICMP:
+			count_icmp++;
+			return;
+		default:
+			count_other++;
+			return;
+	}
 }
 
 
@@ -106,8 +125,6 @@ void processor(u_char *arg,const struct pcap_pkthdr* pkthdr,const u_char* packet
 
 int main (int argc, char *argv[])
 {
-
-	
 	pcap_t *pcap;
 	const unsigned char *packet;
 	char errbuf[PCAP_ERRBUF_SIZE];
@@ -132,6 +149,7 @@ int main (int argc, char *argv[])
 	int count_tcp = 0;
 	int count_udp = 0;
 	int count_icmp = 0;
+	int count_other = 0;
 //////////////////////////// all the counts .. ///////////////////////
 
 
@@ -172,20 +190,6 @@ int main (int argc, char *argv[])
 
 
 ///////////////////////// parser ends //////////////////////////////////
-
-
-
-
-
-	pcap_lookupnet(argv[1],&net,&mask,errbuf);
-
-	pcap = pcap_open_offline(argv[1],errbuf);
-
-	if(pcap == NULL)
-	{
-		printf("err: %s",errbuf);
-		return 0;
-	}
 
 /*
 	if(pthread_create(&th_pcap,NULL,capture,0))
@@ -238,6 +242,8 @@ int main (int argc, char *argv[])
 		packet_count++;
 	
 		while(pcap_next_ex(pcap,&header,&packet)>0){
+
+
 	
 			byte_count += header->len;
 			packet_count++;
